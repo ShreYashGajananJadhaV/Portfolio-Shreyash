64dd4816a91860278e3c233eb58963bfb23777a1
Shreyash Gajanan Jadhav <sj2454925@gmail.com>
2024-06-29 14:08:55 +0530

    dfdf

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,15 +4,15 @@ function Experience() {
   return (
     <div className="Experience shadow-2xl w-full rounded-xl border-2 border-white text-white p-5 mt-4 flex  ">
       <section className="mr-2 w-1/2">
-        <div className="w-full mx-auto p-4 rounded-3xl bg-white border-2 border-gray-600">
+        <div className="w-full mx-auto p-4 rounded-3xl bg-white border-2 border-gray-600 shadow-lg">
           <div className="flex flex-col mb-4">
-            <h1 className="text-lg font-semibold  text-blue-500">Company:</h1>
-            <p className="text-base font-semibold  text-stone-600 ">
+            <h1 className="text-lg font-semibold  text-stone-600">Company:</h1>
+            <p className="text-base font-semibold  text-stone-500 ">
               Atlas Primary
             </p>
           </div>
           <div className="flex flex-col mb-4">
-            <h1 className="text-lg font-semibold  text-indigo-500">
+            <h1 className="text-lg font-semibold  text-stone-600">
               Designation:
             </h1>
             <p className="text-base  hover:text-stone-700 text-stone-500">
@@ -20,7 +20,7 @@ function Experience() {
             </p>
           </div>
           <div className="flex flex-col mb-4">
-            <h1 className="text-lg font-semibold  text-indigo-500">
+            <h1 className="text-lg font-semibold  text-stone-600">
               Technology:
             </h1>
             <p className="text-base  hover:text-stone-700 text-stone-500">
@@ -29,25 +29,25 @@ function Experience() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col mt-2 bg-red-400 rounded-3xl p-2 border-2 border-gray-600">
+        <div className="flex flex-col mt-2 bg-red-400 rounded-3xl p-4 border-2 border-gray-600">
           <h2 className="text-lg font-semibold  text-white mb-4">
             Additional:
           </h2>
 
-          <p className="leading-relaxed text-base  text-white ml-4">
+          <p className="leading-relaxed text-base  text-white ">
             Utilized Selenium, Selenide, Gherkin, and TestNG frameworks to
             automate frontend testing processes, enhancing the efficiency and
             accuracy of the testing phase.
           </p>
         </div>
       </section>
-      <section className="w-[70%] bg-blue-500 rounded-3xl p-2 border-2 border-gray-600">
-        <div className=" flex flex-col mb-4 ">
+      <section className="w-[70%] bg-blue-400 rounded-3xl p-2 border-2 border-gray-600 shadow-lg">
+        <div className=" flex flex-col mb-4 p-4">
           <h2 className="text-lg font-semibold  text-white mb-4">
             Description:
           </h2>
 
-          <p className=" leading-relaxed text-base  text-white ml-4">
+          <p className=" leading-relaxed text-base  text-white ">
             Designed and implemented various microservices, ensuring seamless
             integration and high performance. Applied robust security measures
             using Spring Security to protect the application from potential
